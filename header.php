d5c87996d859cada846f93c8a7ff37726e5ec63d
Lada-Starodubtseva <112096860+Lada-Starodubtseva@users.noreply.github.com>
2023-05-23 09:43:17 -0400

    Ajout de template-atelier

--- header.php
+++ header.php
@@ -9,6 +9,7 @@
 
 
     <?php wp_head(); ?>
+    
 </head>
 
 <body class= "custom-background site" site <?php echo(is_front_page() || is_404() ? "no-aside": "") ?> ">
@@ -41,13 +42,35 @@
 
         </section>
         <?= get_search_form() ?>
+      
         <h1> <a class="site_titre" href="<?= bloginfo('url'); ?>"> <?= bloginfo('name'); ?></a> </h1>
         <h2 class="site__description"><?= bloginfo('description'); ?> </h2>
-    </header>
 
+
+        
+    </header>
+ 
 
 
     <?php 
 
 if(! is_front_page() && ! is_404()){
-get_template_part("template-parts/aside");} ?>
+get_template_part("template-parts/aside");} 
+
+
+
+
+$nouvelle_classe = "";
+if  (is_front_page() || 
+    (! in_category("cours") && ! in_category("4w4")) ){
+      $nouvelle_classe = 'no-aside';
+    }
+if (is_page_template('template-atelier.php')) {
+     $nouvelle_classe = '';
+}
+
+
+
+
+?>
+
