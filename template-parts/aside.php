2a8b295daf4e8eac856bf1386daa839aae8564e7
Lada-Starodubtseva <112096860+Lada-Starodubtseva@users.noreply.github.com>
2023-05-23 10:55:53 -0400

    Switch entre le menu de base et le menu des Ateliers

--- template-parts/aside.php
+++ template-parts/aside.php
@@ -13,6 +13,7 @@
             if(in_category('cours')){
                 $menu = "cours";
             }
+           
             //menu peut prendre les valeurs : "4w4", "cours" 
             echo $menu;
             wp_nav_menu(array(
@@ -20,5 +21,32 @@
                 "container" => "nav"
                 
             )) 
+
+           
+           
         ?>
+
+        
+</aside>
+
+<aside class="aside-ateliers">
+    
+<?php 
+         
+        
+         if(get_template('Ateliers')){
+             $menu = "ateliers";
+         }
+        
+         //menu peut prendre les valeurs : "4w4", "cours" 
+         echo $menu;
+         wp_nav_menu(array(
+             "menu" => $menu,
+             "container" => "nav"
+             
+         )) 
+
+        
+        
+     ?>
 </aside>
